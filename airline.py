58e6a4997a519f02cf2481286e7497efea2848d7
Eugene Chew <echew140899@gmail.com>
2024-01-20 12:32:06 +0800

    Finished Flight and passenger classes

--- airline.py
+++ airline.py
@@ -23,8 +23,40 @@ class Flight:
             "non_member": PriorityQueue()
         }
 
+    def get_passengers_info(self) -> List[Passenger]:
+        return self.confirmed_passengers
+
+    def add_passenger(self, passenger:Passenger):
+
+        # if there is space in the confirmed_passengers list, add passengers to the list.
+        # Else, add passengers to the queue
+
+        if len(self.confirmed_passengers) < self.max_capacity:
+            self.confirmed_passengers.append(passenger)
+        else:
+            membership_status = passenger.membership_status
+            self.waiting_list[membership_status].put(passenger)
+
+    def assign_seat_to_waiting_list(self):
+        # Assign seats to passengers on waiting lists based on membership
+        for status in ["gold", "sliver", "non_member"]:
+            if not self.waiting_list[status].empty():
+                next_passenger = self.waiting_list[status].get()
+                self.confirmed_passengers.append(next_passenger)
+                print(f"Seat assigned to {next_passenger.name} on flight {self.flight_number}")
+
+    def occupany_rate(self) -> float:
+        return len(self.confirmed_passengers)/self.max_capacity
+
+    class AirLineReservationSystem:
+        pass
+
+
 def main():
-    pass
+    # Create flights
+    flight1 = Flight("F001", 200, "CityA", "CityB")
+    flight2 = Flight("F002", 150, "CityC", "CityD")
+
 
 if __name__ == "__main__":
     main()
